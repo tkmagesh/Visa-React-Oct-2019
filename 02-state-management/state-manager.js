fe728dc0ed94b73dcb35ea65311b86efc4a49eff
Magesh Kuppan <tkmagesh77@gmail.com>
2019-10-17 10:36:30 +0800

    state manager implementation

---
+++ 02-state-management/state-manager.js
@@ -0,0 +1,36 @@
+let SM = (function(){
+    let _currentState = undefined,
+        _subscribers = [],
+        _reducer = null,
+        _init_action = '@@INIT/ACTION';
+
+    function getState(){
+        return _currentState;
+    }
+
+    function subscribe(fn){
+        _subscribers.push(fn);
+    }
+
+    function dispatch(action){
+        let newState = _reducer(_currentState, action);
+        if ( _currentState === newState ) return;
+        _currentState = newState;
+        triggerChange();
+    }
+
+    function triggerChange(){
+        _subscribers.forEach(fn => fn());
+    }
+
+    function createStore(reducer){
+        _reducer = reducer;
+        
+        //to initialize the store state with valid initial state
+        _currentState = reducer(_currentState, _init_action);
+
+        return { getState, subscribe, dispatch };
+    }
+
+    return { createStore };
+})();
